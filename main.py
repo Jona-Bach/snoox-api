00fec7c4474cf56800a4e29e1c222fb1b8e9d301
D355I <jonabach.kontakt@gmail.com>
2026-09-24 18:36:35 +0200

    Stock-Endpoints: POST, GET all, GET by symbol

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, Depends, Header, HTTPException
 from dotenv import load_dotenv
 import os
+from pydantic import BaseModel
+
 
 load_dotenv()
+
+class Stock(BaseModel):
+    symbol : str
+    name : str
+    price : float
+
+database : dict[str, Stock] = {}
+
 
 API_KEY = os.getenv("API_KEY")
 
@@ -11,7 +21,7 @@ app = FastAPI()
 
 def verify_api_key(x_api_key : str = Header(...)):
     if x_api_key.strip() != API_KEY:
-        raise HTTPException(401, "Api_KEy is not corret")
+        raise HTTPException(401, "Api_Key is not corret")
 
     return {"message":"Welcome to Snoox Api!"}
 
@@ -26,3 +36,19 @@ def send_key(result = Depends(verify_api_key)):
 def read_root():
     return {"message":"Hello from Snoox API"}
 
+@app.post("/stocks")
+def add_stock(stock: Stock):
+    database[stock.symbol] = stock
+
+    return {"stock": f"Added {stock.name}"}
+
+@app.get("/stocks")
+def get_all_stocks():
+    return database
+
+@app.get("/stocks/{symbol}")
+def get_symbol_stock(symbol: str):
+    if symbol in database:
+        return database[symbol]
+    else:
+        raise HTTPException(404, "Symbol not found")
